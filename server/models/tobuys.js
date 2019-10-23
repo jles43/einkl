a0818566a43cc71a7854db256e116912508fe884
Jauhen Leskavets <jauhen.leskavets@allianz.de>
2019-10-23 16:26:11 +0200

    Type in model corrected

--- server/models/tobuys.js
+++ server/models/tobuys.js
@@ -5,7 +5,7 @@ const tobuySchema = new mongoose.Schema({
   what: String,
   where: String,
   when: {type: Date, default: Date.now},
-  count: integer,
+  count: Number,
   done: Date  // null, wenn noch nicht erledigt
 });
 mongoose.model('Tobuy', tobuySchema);

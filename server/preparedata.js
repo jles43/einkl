c68cf7ea9f6e52a7d200727b3dc0804eb7401d3e
Jauhen Leskavets <jauhen.leskavets@allianz.de>
2019-11-04 17:16:55 +0100

    Prepare Data Module created

---
+++ server/preparedata.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const data1 = [
+  {
+    what: 'H-Milch',
+    where: 'Aldi',
+    count: 1
+  },
+  {
+    what: 'Rib-Eye Steak',
+    where: 'Lidl',
+    count: 1
+  },
+  {
+    what: 'Joghurt fettarm',
+    where: 'Lidl',
+    count: 4
+  },
+  {
+    what: 'Kartoffelsalat',
+    where: 'Aldi'
+  },
+  {
+    what: 'Pfeffer schwarz "Wagner"',
+    where: 'Kaufland'
+  },
+  {
+    what: 'Chips "Lays"',
+    count: 2
+  },
+  {
+    what: 'Kiste Tegernseer Bier',
+    where: 'Orterer',
+    count: 1
+  }
+];
